28c12701e97e666c4293134c378fc0c69247bea8
Dmitriy Malakhov <lesnichiy1986@gmail.com>
2023-10-15 22:39:43 +0300

    added available commands and a list of functions

--- notes.py
+++ notes.py
@@ -13,3 +13,40 @@ notes = load_notes()
 def save_notes(notes):
     with open('notes.json', 'w') as file:
         json.dump(notes, file, indent=4)
+
+
+# def print_notes():
+
+# def delete_note():
+
+# def edit_note():
+
+# def add_note():
+
+# def filter_notes():
+
+
+while True:
+    print("Доступные команды для ввода:")
+    print("add - добавить новую заметку")
+    print("edit - изменить существующую заметку")
+    print("delete - удалить существующую заметку")
+    print("filter - отфильтровать заметки по дате")
+    print("print - показать все существующие заметки")
+    print("exit - выйте изпрограммы")
+    command = input("Введите команду: ")
+   
+    if command == "add":
+        add_note()
+    elif command == "edit":
+        edit_note()
+    elif command == "delete":
+        delete_note()
+    elif command == "filter":
+        filter_notes()
+    elif command == "print":
+        print_notes()
+    elif command == "exit":
+        break
+    else:
+        print("Введена некорректная команда")
