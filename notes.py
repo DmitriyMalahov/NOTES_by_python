4dcdf8beac23b7cc81ba62d121154dd48f3161c4
Dmitriy Malakhov <lesnichiy1986@gmail.com>
2023-10-15 22:17:32 +0300

    add def load_notes and save_notes, add file notes.jason

--- notes.py
+++ notes.py
@@ -1,1 +1,15 @@
-print ("hi")
+import json
+import datetime
+
+def load_notes():
+    try:
+        with open('notes.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+notes = load_notes()
+
+def save_notes(notes):
+    with open('notes.json', 'w') as file:
+        json.dump(notes, file, indent=4)
